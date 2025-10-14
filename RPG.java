516fc0bc77e5eed22929a7776a22cdb00b7e09f9
LARISSA FURLAN FERREIRA <larissa.ff3@puccampinas.edu.br>
2025-10-14 09:15:35 -0300

    Criando Classes

---
+++ RPG.java
@@ -0,0 +1,60 @@
+public class  Personagem()
+{
+    char nome;
+    short pontosVida;
+    short ataque;
+    short defesa;
+    short nivel;
+    Inventario inventario;
+    public Personagem(char nome, short pontosVida, short ataque ,short defesa, short nivel, Inventario inventario){
+        nome = this.nome;
+        pontosVida = this.pontosVida;
+        ataque = this.ataque;
+        defesa = this.defesa;
+        nivel = this.nivel;
+        inventario = this.inventario;
+    }
+}
+
+public class Guerreiro extends Personagem(){
+    public Guerreiro(char nome, short pontosVida, short ataque ,short defesa, short nivel, Inventario inventario){
+        nome = this.nome;
+        pontosVida = this.pontosVida;
+        ataque = this.ataque;
+        defesa = this.defesa;
+        nivel = this.nivel;
+        inventario = this.inventario;
+} 
+
+public class Mago extends Personagem(){
+    public Mago(char nome, short pontosVida, short ataque ,short defesa, short nivel, Inventario inventario){
+        nome = this.nome;
+        pontosVida = this.pontosVida;
+        ataque = this.ataque;
+        defesa = this.defesa;
+        nivel = this.nivel;
+        inventario = this.inventario;
+} 
+
+public class Arqueiro extends Personagem(){
+    public Arqueiro(char nome, short pontosVida, short ataque ,short defesa, short nivel, Inventario inventario){
+        nome = this.nome;
+        pontosVida = this.pontosVida;
+        ataque = this.ataque;
+        defesa = this.defesa;
+        nivel = this.nivel;
+        inventario = this.inventario;
+} 
+
+public class Inimigo extends Personagem(){
+    public Inimigo(char nome, short pontosVida, short ataque ,short defesa, short nivel, Inventario inventario){
+        nome = this.nome;
+        pontosVida = this.pontosVida;
+        ataque = this.ataque;
+        defesa = this.defesa;
+        nivel = this.nivel;
+        inventario = this.inventario;
+} 
+public class Inventario(){
+    
+}
